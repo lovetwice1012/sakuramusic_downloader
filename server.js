d81bbbd6c4234e9cf98318de242c77d691dc76d8
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2024-08-14 19:23:19 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,43 @@ const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require("ffmpeg-static")
 const play = require('play-dl');
 const fs = require("fs")
+const cp = require('child_process');
+const stream = require('stream');
+
+// default export: the ffmpeg muxer
+const ytmux = (link, agent, options = {}) => {
+    const result = new stream.PassThrough({ highWaterMark: options.highWaterMark || 1024 * 512 });
+    ytdl.getInfo(link, options).then(info => {
+        audioStream = ytdl(info, {quality: 'highestaudio', agent: agent });
+        videoStream = ytdl(info, {quality: 'highestvideo', agent: agent });
+        // create the ffmpeg process for muxing
+        ffmpegProcess = cp.spawn(ffmpegPath, [
+            // supress non-crucial messages
+            '-loglevel', '8', '-hide_banner',
+            // input audio and video by pipe
+            '-i', 'pipe:3', '-i', 'pipe:4',
+            // map audio and video correspondingly
+            '-map', '0:a', '-map', '1:v',
+            // no need to change the codec
+            '-c', 'copy',
+            // output mp4 and pipe
+            '-f', 'matroska', 'pipe:5'
+        ], {
+            // no popup window for Windows users
+            windowsHide: true,
+            stdio: [
+                // silence stdin/out, forward stderr,
+                'inherit', 'inherit', 'inherit',
+                // and pipe audio, video, output
+                'pipe', 'pipe', 'pipe'
+            ]
+        });
+        audioStream.pipe(ffmpegProcess.stdio[3]);
+        videoStream.pipe(ffmpegProcess.stdio[4]);
+        ffmpegProcess.stdio[5].pipe(result);
+    });
+    return result;
+};
 const agentOptions = {
     headers: {
         referer: "https://www.youtube.com/",
